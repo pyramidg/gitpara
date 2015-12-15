4e1398d243a733e97652c942cf91f0eb39a934a0
studioplace <webstudioplace.com>
2015-12-15 12:53:30 -0500

    update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var modernizr = require("modernizr");
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
